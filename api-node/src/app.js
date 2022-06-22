5a6f113ed74ce949dfd0c883c721f2a9533e303b
Alisson Buarque <sr.alissombuarque@gmail.com>
2022-06-22 13:03:29 -0300

    Segundo Commit Executando Extenções / Criando Banco de Dandos

--- api-node/src/app.js
+++ api-node/src/app.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+import {openDb} from './configDB.js';
+
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+openDb();
+
+app.get("/", function(req, res){
+    res.send("Hello World");
+});
+
+app.post("/pessoa", function(req, res){
+    console.log(req.body);
+    res.json({
+        "statucCode": 200
+    })
+});
+
+app.listen(3000, ()=>console.log("Api Rodando"))
