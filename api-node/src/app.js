5faaf9acc8ffb4ffcf06eef6806d350a7b3d5e92
Alisson Buarque <sr.alissombuarque@gmail.com>
2022-06-24 15:35:13 -0300

    Commit Implementando PUT, UPDATE e DELETE

--- api-node/src/app.js
+++ api-node/src/app.js
@@ -1,20 +1,47 @@
-import {openDb} from './configDB.js';
+// import {openDb} from './configDB.js'; //
+import { createTable, insertPessoa, updatePessoa, selectPessoa, selectPessoas } from './controler/pessoa.js';
 
 import express from 'express';
 const app = express();
 app.use(express.json());
 
-openDb();
+createTable();
 
 app.get("/", function(req, res){
-    res.send("Hello World");
+    res.send("Api Rodando");
+});
+
+app.get("/pessoa", async function(req, res){
+    let pessoas = await selectPessoas();
+    res.json(pessoas);
+});
+
+app.get("/pessoa", async function(req, res){
+    let pessoa = await selectPessoa(req.body.id);
+    res.json(pessoa);
 });
 
 app.post("/pessoa", function(req, res){
-    console.log(req.body);
+    insertPessoa(req.body)
     res.json({
         "statucCode": 200
     })
 });
+
+app.put('/pessoa', function(req, res){
+
+    if(req.body && !req.body.id){
+        res.json({
+            "statusCode":"400",
+            "msg":"Você precisa informar um id"
+        })
+    }else{
+        updatePessoa(req.body)
+        res.json({
+            "statucCode": 200
+        })
+    }
+});
+
 
 app.listen(3000, ()=>console.log("Api Rodando"))
